2bd3398c434be8449d648b46c046f0f02420cd66
Anton Mayatskiy <worknekki@gmail.com>
2019-06-19 17:55:57 +1000

    Structure of js files changed

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,11 +1,7 @@
-// const $ = require('jquery') ;
-// const svg4everybody = require('svg4everybody');
-// const togglePickPlace = require('./modules/pick-place');
-// const toggleMobileMenu = require('./modules/mobile-menu');
 import $ from 'jquery';
 import svg4everybody from 'svg4everybody';
-import pickPlace from './modules/pick-place';
-import mobileMenu from './modules/mobile-menu';
+import pickPlace from './modules/pick-place/pick-place';
+import mobileMenu from './modules/mobile-menu/mobile-menu';
 import baron from './modules/baron/baron';
 
 $(document).ready(function () {

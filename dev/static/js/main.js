74077e133430789b7a4c5681c3298ca78283d2d6
Anton Mayatskiy <worknekki@gmail.com>
2019-06-20 12:51:47 +1000

    css classes in header replaced

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 import svg4everybody from 'svg4everybody';
 import pickPlace from './modules/pick-place/pick-place';
 import mobileMenu from './modules/mobile-menu/mobile-menu';
-import mainSlider from './modules/main-slider/main-slider';
+// import mainSlider from './modules/main-slider/main-slider';
 import baron from './modules/baron/baron';
 
 $(document).ready(function () {

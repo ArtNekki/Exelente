f5ec56e08f123f549796c8bd86baf079583c7389
Anton Mayatskiy <worknekki@gmail.com>
2019-06-21 14:51:26 +1000

    main slider for desktop finished

--- dev/static/js/modules/main-slider/main-slider.js
+++ dev/static/js/modules/main-slider/main-slider.js
@@ -1,8 +1,24 @@
 import Swiper from 'swiper';
 
+function formatFraction(number) {
+  return number < 10 ? '0 ' + number : number;
+}
+
 var swiper = new Swiper('.swiper-container', {
+     slidesPerView: 'auto',
      navigation: {
        nextEl: '.swiper-button-next',
        prevEl: '.swiper-button-prev',
-     }
+     },
+     pagination: {
+        el: '.main-slider__pagination',
+        type: 'fraction',
+        renderFraction: function (currentClass, totalClass) {
+            return '<span class="main-slider__pagination-num ' + currentClass + '"></span>' +
+                   '<span class="main-slider__pagination-num ' + totalClass + '"></span>';
+        },
+        formatFractionCurrent: formatFraction,
+        formatFractionTotal: formatFraction
+      },
+
    });

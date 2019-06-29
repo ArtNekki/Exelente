f30f739f1fa0b672883e91aa8bfc9996a393cf09
Anton Mayatskiy <worknekki@gmail.com>
2019-06-29 18:52:01 +1000

    Bug in coutries-slider fixed

--- dev/static/js/modules/slider/slider.js
+++ dev/static/js/modules/slider/slider.js
@@ -8,7 +8,7 @@ const breakpointChecker = function() {
       console.log(breakpoint.matches)
 
    if ( breakpoint.matches === true && swiper !== undefined ) {
-      swiper.destroy( true, false );
+      swiper.destroy( true, true );
       return;
 
    } else if ( breakpoint.matches === false ) {
@@ -18,26 +18,9 @@ const breakpointChecker = function() {
 
 const enableSwiper = function () {
   swiper = new Swiper('#countrySlider', {
-    // autoHeight: true,
-    // slidesPerView: 1,
-    // spaceBetween: 0,
-    // breakpoints: {
-    //   480: {
-    //     slidesPerView: 1,
-    //     spaceBetween: 0
-    //   },
-    //   768: {
-    //     slidesPerView: 1,
-    //     spaceBetween: 0
-    //   },
-    //   1280: {
-    //     slidesPerView: 9,
-    //     spaceBetween: 0
-    //   }
-    // },
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: '.slider--countries .slider__btn--next',
+      prevEl: '.slider--countries .slider__btn--prev',
    }
  });
 }
